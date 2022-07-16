96797067676fb70016ede101e887b73143fe9310
18520058 <18520058@ms.uit.edu.vn>
2022-07-16 22:16:10 +0700

    v0.8: create pages for signature management

--- src/pages/TellerOperation/CustomerManagement/EnquiryCustomer.js
+++ src/pages/TellerOperation/CustomerManagement/EnquiryCustomer.js
@@ -89,14 +89,14 @@ function EnquiryCustomer() {
                                     key={row.name}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                     >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="right">{row.calories}</TableCell>
+                                        <TableCell align="right">{row.fat}</TableCell>
+                                        <TableCell align="right">{row.carbs}</TableCell>
+                                        <TableCell align="right">{row.protein}</TableCell>
+                                    </TableRow>
           ))}
         </TableBody>
       </Table>
