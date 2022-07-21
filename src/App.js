959ae1478546d59f059ef00e1adfaadf03dc3228
nksot21 <20520225@gm.uit.edu.vn>
2022-07-21 15:49:01 +0700

    m

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import React, { useCallback, useEffect, useState } from 'react'
 import getDataFunc from './getData';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 
+const express = require('express')
+const path = require('path');
+
+
 
 // const [bio, setBio] = useState([]);
     
@@ -122,6 +126,14 @@ function Example(){
     </QueryClientProvider>
   );
 }
+
+const app = express()
+const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.listen(port, () => console.log(`App is live on port ${port}!`))
+
 
 export default App;
 
